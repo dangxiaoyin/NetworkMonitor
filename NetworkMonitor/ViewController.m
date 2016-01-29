1b58741e6b73f061bb0c681ec92dc0a7a61f511e
dangxiaoyin <dangxy@promote.cache-dns.local>
2016-01-29 14:32:33 +0800

    网络监测：区分2/3/4G网络

--- NetworkMonitor/ViewController.m
+++ NetworkMonitor/ViewController.m
@@ -7,21 +7,79 @@
 //
 
 #import "ViewController.h"
+#import "CoreStatus.h"
+#import "CALayer+Transition.h"
 
-@interface ViewController ()
+@interface ViewController ()<CoreStatusProtocol>
+
+@property (strong, nonatomic) UILabel *showLabel;
+
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"网络监测";
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    /** 开始网络监听 */
+    [CoreStatus beginNotiNetwork:self];
+
+    
+    self.showLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 200, self.view.frame.size.width-40, 100)];
+    self.showLabel.backgroundColor = [UIColor whiteColor];
+    self.showLabel.textColor = [UIColor blackColor];
+    self.showLabel.font = [UIFont boldSystemFontOfSize:30];
+    self.showLabel.textAlignment = NSTextAlignmentCenter;
+    
+    /** 获取当前网络状态：字符串 */
+    NSString * statusString = [CoreStatus currentNetWorkStatusString];
+    self.showLabel.text = statusString;
+
+    [self.view addSubview:self.showLabel];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma mark - CoreStatusProtocol
+
+/** 网络状态变更 */
+-(void)coreNetworkChangeNoti:(NSNotification *)noti;
+{
+    NSString * statusString = [CoreStatus currentNetWorkStatusString];
+    
+    NSLog(@"%@\n\n\n\n=========================\n\n\n\n%@",noti,statusString);
+    
+    [self showText];
+
 }
+
+/**  监测结果 */
+-(void)showText{
+    
+    /** 获取当前网络状态：字符串 */
+    NSString * statusString = [CoreStatus currentNetWorkStatusString];
+    self.showLabel.text = statusString;
+    
+    /**  转场动画 */
+    [self.showLabel.layer transitionWithAnimType:TransitionAnimTypeRamdom subType:TransitionSubtypesFromRamdom curve:TransitionCurveRamdom duration:.5f];
+}
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+    /** 获取当前网络状态：字符串 */
+    NSString * statusString = [CoreStatus currentNetWorkStatusString];
+    NSLog(@"%@",statusString);
+}
+
+
+
+
 
 @end
